14728cdcd439888718077fff47206dd370b46956
Sam Willis <sam.willis@gmail.com>
2024-05-17 12:05:50 +0100

    Multiple lists

--- db/migrations/01-create_items_table.sql
+++ db/migrations/01-create_items_table.sql
@@ -9,14 +9,18 @@
  * See https://electric-sql.com/docs/usage/data-modelling for more information.
  */
 
--- Create a simple items table.
-CREATE TABLE IF NOT EXISTS items (
-  id TEXT PRIMARY KEY NOT NULL,
-  task TEXT NOT NULL,
-  done BOOLEAN NOT NULL,
+CREATE TABLE IF NOT EXISTS lists (
+  id UUID PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL
 );
+ALTER TABLE lists ENABLE ELECTRIC;
 
--- ⚡
--- Electrify the items table
+CREATE TABLE IF NOT EXISTS items (
+  id UUID PRIMARY KEY NOT NULL,
+  task TEXT NOT NULL,
+  done BOOLEAN NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  list_id UUID NOT NULL REFERENCES lists(id) ON DELETE CASCADE
+);
 ALTER TABLE items ENABLE ELECTRIC;
